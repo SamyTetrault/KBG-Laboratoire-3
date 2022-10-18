1f5adcaa06dbf437d3c253ff35b7c9738eb798de
SamyTetrault <101147474+SamyTetrault@users.noreply.github.com>
2022-10-18 13:35:05 -0400

    ldsfnldsknf

---
+++ models/nouvelles.js
@@ -0,0 +1,19 @@
+const Model = require('./model');
+module.exports =
+    class Nouvelle extends Model {
+        constructor(category,title,text,imageUrl,date) {
+            super();
+            this.Category = category !== undefined ? category : "";
+            this.Title = title !== undefined ? titre : "";
+            this.Text = text !== undefined ? texte : "";
+            this.ImageUrl = imageUrl !== undefined ? imageUrl : "";
+            this.Date = date !== undefined ? date : "";
+
+            this.setKey("Titre");
+            this.addValidator('Category', 'string');
+            this.addValidator('Title', 'string');
+            this.addValidator('Text', 'string');
+            this.addValidator('ImageUrl', 'string');
+            this.addValidator('Date', 'string');
+        }
+    }
